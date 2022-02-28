47a0043a175af2f21a6ac6be9f5880d631899225
Kasia Gruszczynska <kasia.gruszczynsk@gmail.com>
2022-02-28 02:37:39 +0100

    enhanced tsp

--- komiwojazer.py
+++ komiwojazer.py
@@ -4,10 +4,12 @@
 #
 # TSP - Problem komiwojazera
 #
+from operator import truediv
 import sys
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import datetime
 
 # ile miast, default N = 100
 N = 100
@@ -28,7 +30,7 @@ def generatePopulation(ile_osobnikow):
         # print("+ nowy osobnik", i)
         population.append(random.sample(range(0, N, 1), N))
         i += 1
-    print("Poczatkowa populacja osobników: ", len(population))
+    print(datetime.datetime.now(), "Poczatkowa populacja osobnikow: ", len(population))
     # print(population)
     return population
 
@@ -41,7 +43,7 @@ def fnDopasowania(population, costTable, n):
         # print(population[n][i], "->", population[n][i+1])
         cost += costTable[population[n][i], population[n][i+1]]
         # print(costTable[population[n][i], population[n][i+1]])
-    print("Koszt podrozy: ", cost)
+    # print("Koszt podrozy: ", cost)
     return cost
 
 # policz koszty dla wszystkich osobnikow w populacji
@@ -56,29 +58,47 @@ def minKosztPodrozy(population, costTable):
         # print("Koszt dla osobnika", n ,":", kosztyOsobnikow[n])
     # print("Koszty osobnikow", kosztyOsobnikow)
     minKoszt = min(kosztyOsobnikow)
-    print("Koszt minimalny/najkrotsza trasa", minKoszt)
+    print(datetime.datetime.now(), "Koszt minimalny/najkrotsza trasa", minKoszt)
     return minKoszt
 
 # selekcja turniejowa
 # Binary tournament selection (BTS)
-def binTounamentSelection(population, costTable):
+# enhancement - zaleznie od parametru passThreshold:
+# rodzic juz nie moze byc drugi raz rodzicem, usuwam z populacji potencjalnych rodzicow
+def binTounamentSelection(population, costTable, passThreshold):
     # do turnieju 1:1 wylosowac osobnikow
     pair = random.sample(range(0, len(population)), 2)
     # print("para", pair)
+
+    print(datetime.datetime.now(), "IN --- binTounamentSelection -- liczba w populacji", len(population))
     # print("population", population)
 
     if fnDopasowania(population, costTable, pair[0]) < fnDopasowania(population, costTable, pair[1]):
-        rodzic = population[pair[0]].copy()
+        rodzic = population[pair[0]]
         # print("rodzic to osobnik nr", pair[0], population[pair[0]])
-        # print("osobnik usuniety nr", pair[1], population[pair[1]])
-        del population[pair[1]]
+        if passThreshold == False:
+            # print("osobnik usuniety nr", pair[1], population[pair[1]])
+            del population[pair[1]]     # osobnik usuniety przez fn dopasowania
     else:
-        rodzic = population[pair[1]].copy()
-        # print("rodzic to osobnik nr", pair[1], population[pair[1]])
-        # print("osobnik usuniety nr", pair[0], population[pair[0]])
-        del population[pair[0]]
+        rodzic = population[pair[1]]
+        # print("else rodzic to osobnik nr", pair[1], population[pair[1]])
+        if passThreshold == False:
+            # print("else osobnik usuniety nr", pair[0], population[pair[0]])
+            del population[pair[0]]     # osobnik usuniety przez fn dopasowania
 
-    print(population)
+    if passThreshold == True:
+        # print("======= usun pare")
+        if pair[0] < pair[1]:
+            del population[pair[1]] # juz nie moze byc drugi raz rodzicem, usuwam z populacji potencjalnych rodzicow
+            del population[pair[0]] # osobnik usuniety przez fn dopasowania
+        else:
+            del population[pair[0]] # osobnik usuniety przez fn dopasowania
+            del population[pair[1]] # juz nie moze byc drugi raz rodzicem, usuwam z populacji potencjalnych rodzicow
+
+    print(datetime.datetime.now(), "OUT --- binTounamentSelection -- liczba w populacji", len(population))
+    # print("population", population)
+    # print(datetime.datetime.now(), "OUT --- binTounamentSelection -- liczba rodzicow SO FAR", len(rodzic))
+    # print("rodzic", rodzic)
 
     return rodzic
 
@@ -135,27 +155,34 @@ def crossOver(rodzic1_val, rodzic2_val):
 # z puli rodzicow bierz pary
 def krzyzowanieRodzicow(rodzice):
     newGeneration=[]
-
     for i in range(len(rodzice)-1):
-        print("krzyzowanie")
         dzieci = crossOver(rodzice[i],rodzice[i+1])
         newGeneration.append(dzieci[0])
         newGeneration.append(dzieci[1])
     return newGeneration
 
 # ewolucja, krzyzowanieRodzicow wywolaj dla populacji
-def evolution(population, rodzice, costTable):
+def evolution(population, rodzice, costTable, passThreshold):
+
+    print(datetime.datetime.now(), "----> na wejsciu population, liczba osobnikow:", len(population))
+    print(datetime.datetime.now(), "----> na wejsciu rodzicow, liczba osobnikow:", len(rodzice))
+
     # wybranie puli rodzicow
     while len(population) >= 2:
-        # print("while len populacji:", len(population), population)
-        rodzice.append(binTounamentSelection(population, costTable))
+        rodzice.append(binTounamentSelection(population, costTable, passThreshold))
 
-    print("pula rodzicow:", len(rodzice), rodzice)
-    # print("ile osobnikow zostalo w populacji poczatkowej - wygina!:", len(population), population)
+    print(datetime.datetime.now(), "Pula rodzicow, liczba osobnikow:", len(rodzice))
+    print(datetime.datetime.now(), "ile osobnikow zostalo w populacji poczatkowej - wygina!:", len(population))
+
+    print(datetime.datetime.now(), "----> PRZED CROSSOVER population, liczba osobnikow:", len(population))
+    print(datetime.datetime.now(), "----> PRZED CROSSOVER rodzicow, liczba osobnikow:", len(rodzice))
 
     newGeneration = krzyzowanieRodzicow(rodzice)
 
-    # print("evolution ---> newGeneration", newGeneration)
+    print(datetime.datetime.now(), "DZIECI po krzyzowanie Rodzicow ---> new Generation", len(newGeneration))
+
+    print(datetime.datetime.now(), "----> na OUT population, liczba osobnikow:", len(population))
+    print(datetime.datetime.now(), "----> na OUT rodzicow, liczba osobnikow:", len(rodzice))
 
     # zwroc dzieci czyli nowe pokolenie
     return newGeneration
@@ -181,7 +208,7 @@ def mutation(newGeneration, mutationRate):
     # return mutated generation
     return newGeneration
 
-def nextGeneration(population, costTable):
+def nextGeneration(population, costTable, passThreshold):
     rodzice = []
     newGeneration = []
 
@@ -189,7 +216,7 @@ def nextGeneration(population, costTable):
     # print("len populacji:", len(population), population)
 
     # ewolucja przez krzyzowanie osobnikow - jednopunktowe
-    newGeneration = evolution(population, rodzice, costTable)
+    newGeneration = evolution(population, rodzice, costTable, passThreshold)
 
     # mutacja - 1:1000 osobników
     # 1000 / 100 = 10 czyli co ~10ty osobnik mutuje
@@ -198,20 +225,22 @@ def nextGeneration(population, costTable):
 
     if (mutationRate < len(newGeneration)):
         mutation(newGeneration, mutationRate)
-        print("zmutowana generacja", newGeneration)
+        # print("zmutowana generacja", newGeneration)
     else:
-        print("za duzy mutationRate!")
+        print(datetime.datetime.now(), "za duzy mutationRate!")
 
     # print("nextGeneration ----> newGeneration:", newGeneration)
 
     return newGeneration
 
 # run geneticAlgorithm dla x pokolen
-def geneticAlgorithm(ile_osobnikow, generations):
+def geneticAlgorithm(ile_osobnikow, generations, threshold):
 
     print("Liczba miast:", N)
     print("Liczba osobnikow poczatkowych:", ile_osobnikow)
-    print("Liczba pokolen:", generations)
+    print("Liczba pokolen:", generations) 
+    passThreshold = False         
+
     # generacja tabeli kosztow podrozy
     costTable = []
     print("\n-------- Tabela kosztow podrozy (10$ - 99$) --------")
@@ -220,32 +249,36 @@ def geneticAlgorithm(ile_osobnikow, generations):
 
     # generuj populacje poczatkowa 
     population = generatePopulation(ile_osobnikow)
-    print("Ilosc osobnikow w populacji:", len(population))
+    print(datetime.datetime.now(), "Ilosc osobnikow w populacji:", len(population))
 
     # koszty podrozy dla wszystkich osobnikow w poczatkowej populacji
     # i koszt minimalny/najkrotsza trasa w poczatkowej populacji
     minKoszt = minKosztPodrozy(population, costTable)
-    print("Koszt minimalny/najkrotsza trasa w poczatkowej populacji:", minKoszt)
+    print(datetime.datetime.now(), "Koszt minimalny/najkrotsza trasa w poczatkowej populacji:", minKoszt)
 
     progress = []
     progress.append(minKoszt)
-
-    print("Tworzenie nowego pokolenia")
+ 
+    print(datetime.datetime.now(), "Tworzenie kolenych pokolen")
     nowePokolenie = population.copy()
-    print("nowePokolenie = population", nowePokolenie)
+    print(datetime.datetime.now(), "generations -- liczba osobników w 0 -> 1 pokoleniu", len(nowePokolenie))
+    # print("nowePokolenie = population", nowePokolenie)
 
     for i in range(0, generations):
-        print("pokolenie nr", i+1)
-        nowePokolenie = nextGeneration(nowePokolenie, costTable)
-        print("generations -----> nowePokolenie", nowePokolenie)
+        print(datetime.datetime.now(), "-- pokolenie nr", i+1)
+        if threshold > 0 and threshold >= i+1:
+            passThreshold = True
+        nowePokolenie = nextGeneration(nowePokolenie, costTable, passThreshold)
+        # print("generations -----> nowePokolenie", nowePokolenie)
+        print(datetime.datetime.now(), "-- generations -- liczba osobników w nowym pokoleniu", len(nowePokolenie))
         minKoszt = minKosztPodrozy(nowePokolenie, costTable)
-        print("Koszt minimalny/najkrotsza trasa dla pokolenia nr", i+1 ,":", minKoszt)
+        print(datetime.datetime.now(), "Koszt minimalny/najkrotsza trasa dla pokolenia nr", i+1 ,":", minKoszt)
         progress.append(minKoszt)
 
     # koszty podrozy dla wszystkich osobnikow w ostatnim pokoleniu populacji
     # i koszt minimalny/najkrotsza trasa w ostatnim pokoleniu populacji
     # minKoszt = minKosztPodrozy(population, costTable)
-    print("Koszt minimalny/najkrotsza trasa w ostatnim pokoleniu populacji:", minKoszt)
+    print(datetime.datetime.now(), "Koszt minimalny/najkrotsza trasa w ostatnim pokoleniu populacji:", minKoszt)
 
     plt.plot(progress)
     plt.title('Problem komiwojazera')
@@ -262,9 +295,16 @@ def geneticAlgorithm(ile_osobnikow, generations):
 # ile osobnikow, default=50
 # ile pokolen, default=500
 
+# zaleznie od threshold,
+# rodzic juz nie moze byc drugi raz rodzicem, usuwany z populacji potencjalnych rodzicow:
+# threshold = 0
+# powyzej tej liczby pokolen bedzie usuwany rodzic:
+# threshold = 15
+
 # argumenty podane z command line'a
 ile_osobnikow = int(sys.argv[1])
 generations = int(sys.argv[2])
+threshold = int(sys.argv[3])
 
 # run everything and draw a plot: Koszt podrozy per Generation
-geneticAlgorithm(ile_osobnikow, generations)
+geneticAlgorithm(ile_osobnikow, generations, threshold)
